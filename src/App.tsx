9d9cc943d6e1850fdc54a9b6e5f5dff6352d64f1
joaobr200 <joaoarantesjob@gmail.com>
2022-06-24 05:48:36 -0300

    add Provieders

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import Home from "./pages/index";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./lib/apollo";
+import Ignite from "./pages/Ignite";
+import Login from "./pages/login";
+import { MenuContextProvider } from "./context/MenuContext";
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <MenuContextProvider>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/ignite" element={<Ignite />} />
+            <Route path="/ignite/lesson/:slug" element={<Ignite />} />
+          </Routes>
+        </BrowserRouter>
+      </ApolloProvider>
+    </MenuContextProvider>
   );
 };
 
